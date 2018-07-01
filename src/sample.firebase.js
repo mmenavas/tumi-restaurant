312425021e214f7e67f1b7f1590a4d0cf6dd031a
Maximo Mena <maximo.mena@gmail.com>
2018-06-30 18:18:13 -0700

    Back up data and add sample.firebase.js

---
+++ src/sample.firebase.js
@@ -0,0 +1,16 @@
+// Fill out firebase account information below and
+// copy this file to firebase.js
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
