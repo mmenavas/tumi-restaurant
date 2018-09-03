30c81bed5c30bfb4e7d98673a56ce98dd82064bd
Maximo Mena <maximo.mena@gmail.com>
2018-09-03 13:25:41 -0700

    Load carbon components CSS directly from static assets
    - Update description metatag

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -2,22 +2,19 @@ import React from 'react';
 import './MenuItem.css';
 
 const MenuItem = props => (
-  // <Tile>
-    <div className="menu-item">
-      <div className="menu-item__header">
-        <div className="menu-item__name">
-          {props.name}
-        </div>
-        <div className="menu-item__price">
-          {props.price}
-        </div>
+  <div className="menu-item">
+    <div className="menu-item__header">
+      <div className="menu-item__name">
+        {props.name}
       </div>
-      <div className={"menu-item__description" + (!props.showDescription ? " hide" : "")}>
-        {props.description}
+      <div className="menu-item__price">
+        {props.price}
       </div>
     </div>
-  // </Tile>
-
+    <div className={"menu-item__description" + (!props.showDescription ? " hide" : "")}>
+      {props.description}
+    </div>
+  </div>
 );
 
 export default MenuItem;
